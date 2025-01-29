199bd6c5e54ff563e913f19465e39ed98f346da6
tsfranklin <franklintorressantos@gmail.com>
2025-01-29 09:45:05 +0100

    subida 29-01-2025

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a6/c02ac7ba7ddd001f1851ecdea1518b07
@@ -0,0 +1,5 @@
+package servicios;
+
+public interface MenuInterfaz {
+
+}
---
+++ edu.franklintorressantos/src/servicios/Elemento.java
@@ -0,0 +1,41 @@
+package servicios;
+import java.util.Date;
+import java.util.Scanner;
+public class Elemento {
+	
+	long idElemento;
+	private String codigoElemento;
+	private String nombreelemento;
+	private String descripcionElemento;
+	int cantidadElemento;
+	String fchAlta;
+	String fchaBaja;
+	
+	
+	///String codigoElemento = String.valueOf(idElemento);
+	
+	
+	//constructor 
+	
+	public Elemento(long id,String nombre, int cantidad){
+		this.idElemento= id ;
+		this.nombreelemento = nombre;
+		this.cantidadElemento = cantidad;
+		
+		
+	//public void funcionamientoDeAlta();
+	
+	
+		
+		
+	
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
